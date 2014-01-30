066964ea05ce32d48cb8c09354443673ca2e0888
Irhas Faisal <irhasfaisal@yahoo.com>
2014-01-30 13:33:13 +0700

    update pengerjaan

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <cstdlib>
 #include <iostream>
+#include <string>
+#include <cstdio>
+#include <conio.h>
+#include <time.h>
+
 
 using namespace std;
 
@@ -26,9 +31,191 @@ struct Musuh{
     DirectionY pastDirY;
 };
 
-//edit lagi bro
 
-int main(int argc, char *argv[])
+unsigned char MoveMusuh(Musuh& musuh, unsigned char maze[tinggi][lebar], int i, int j, string& smove, bool& nc){
+    if(nc){
+        bool goUp = false;
+        //memungkinkan arah pergerakan ke atas, bawah, samping kanan dan kiri
+        //psb = kemungkinan pergerakan
+        bool psbUp = false;  
+        bool psbDown = false;
+        bool psbRight = false;
+        bool psbLeft = false;
+
+        //pergerakan musuh 
+        if(maze[musuh.y][musuh.x+1] != '#'){//Kanan
+            psbRight = true;
+        }
+        if(maze[musuh.y][musuh.x-1] != '#'){//Kiri
+            psbLeft = true;
+        }
+        if(maze[musuh.y+1][musuh.x] != '#'){//Bawah
+            psbDown = true;
+        }
+        if(maze[musuh.y-1][musuh.x] != '#'){//Atas
+            psbUp = true;
+        }
+
+        //jalur perpindahan
+        enum Mv {up, right, down, left, NA};
+        Mv move = NA;
+
+        //perpindahan sebelumnya
+        if(smove == "up"){
+            move = up;
+        }
+        else if(smove == "right"){
+            move = right;
+        }
+        else if(smove == "down"){
+            move = down;
+        }
+        else if(smove == "left"){
+            move = left;
+        }
+        else{
+            move = NA;
+        }
+
+        if(musuh.pastDirX == Right && !psbRight){
+            if(musuh.pastDirY == Up){
+                if(psbUp){
+                    move = up;
+                }
+                else if(psbDown && move != up){
+                    move = down;
+                }
+                else if(psbLeft){
+                    move = left;
+                }
+                else{
+                    move = down;
+                }
+            }
+            else{
+                if(psbDown){
+                    move = down;
+                }
+                else if(psbUp && move != down){
+                    move = up;
+                }
+                else if(psbLeft){
+                    move = left;
+                }
+                else{
+                    move = up;
+                }
+            }
+        }
+        else if(musuh.pastDirX == Right && psbRight){
+            move = right;
+        }
+        else if(musuh.pastDirX == Left && !psbLeft){
+            if(musuh.pastDirY == Up){
+                if(psbUp){
+                    move = up;
+                }
+                else if(psbDown && move != up){
+                    move = down;
+                }
+                else if(psbRight){
+                    move = right;
+                }
+                else{
+                    move = down;
+                }
+            }
+            else{
+                if(psbDown){
+                    move = down;
+                }
+                else if(psbUp && move != down){
+                    move = up;
+                }
+                else if(psbRight){
+                    move = right;
+                }
+                else{
+                    move = up;
+                }
+            }
+        }
+        else if(musuh.pastDirX == Left && psbLeft){
+            move = left;
+        }
+        else if(musuh.pastDirY == Up && !psbUp){
+            if(musuh.pastDirX == Right){
+                if(psbRight){
+                    move = right;
+                }
+                else if(psbLeft && move != right){
+                    move = left;
+                }
+                else if(psbDown){
+                    move = down;
+                }
+                else{
+                    move = left;
+                }
+            }
+            else{
+                if(psbLeft){
+                    move = left;
+                }
+                else if(psbRight && move != left){
+                    move = right;
+                }
+                else if(psbDown){
+                    move = down;
+                }
+                else{
+                    move = right;
+                }
+            }
+        }
+        else if(musuh.pastDirY == Up && psbUp){
+            move = up;
+        }
+        else if(musuh.pastDirY == Down && !psbDown){
+            if(musuh.pastDirX == Right){
+                if(psbRight){
+                    move = right;
+                }
+                else if(psbLeft && move != right){
+                    move = left;
+                }
+                else if(psbUp){
+                    move = up;
+                }
+                else{
+                    move = left;
+                }
+            }
+            else{
+                if(psbLeft){
+                    move = left;
+                }
+                else if(psbRight && move != left){
+                    move = right;
+                }
+                else if(psbRight){
+                    move = up;
+                }
+                else{
+                    move = right;
+                }
+            }
+        }
+        else if(musuh.pastDirY == Down && psbDown){
+            move = down;
+        }
+}
+}
+
+//belum beres, edit lagi gan
+
+
+int main()
 {
     system("PAUSE");
     return EXIT_SUCCESS;
